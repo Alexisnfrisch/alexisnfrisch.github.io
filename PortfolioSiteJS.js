6d56f609d50b908d32abf5beb01ba52a67a48baa
Lexie <Alexisnfrisch@gmail.com>
2021-06-14 11:58:34 -0400

    Update PortfolioSiteJS.js

--- PortfolioSiteJS.js
+++ PortfolioSiteJS.js
@@ -40,9 +40,6 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   }
 
-})
-
-
   //Get Back to Top Button to Hide
   var backtotopbutton = document.getElementById("scrollToTopBtn");
 
@@ -112,7 +109,7 @@ document.addEventListener('DOMContentLoaded', function() {
     
     var videospan = document.getElementsByClassName("videoclose")[0];
     var videotopause = document.getElementById("vid01");
-    
+
     videospan.onclick = function() {
       videotopause.pause();
       navbar.style.display = "block";
@@ -120,3 +117,5 @@ document.addEventListener('DOMContentLoaded', function() {
       videomodal.style.display = "none";
       backtotopbutton.style.display = "block";
     }
+
+})
